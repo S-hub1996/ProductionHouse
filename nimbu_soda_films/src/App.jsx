54508d2282f2e026dfe879351059c58e1c144b6f
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-07-26 20:41:09 +0530

    new app

---
+++ nimbu_soda_films/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
