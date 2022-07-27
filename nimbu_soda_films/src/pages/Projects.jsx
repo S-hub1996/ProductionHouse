25e8db178a9e583b789b7789eb8378ce3b8513da
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-07-27 19:41:56 +0530

    projects update for mobile

--- nimbu_soda_films/src/pages/Projects.jsx
+++ nimbu_soda_films/src/pages/Projects.jsx
@@ -11,16 +11,16 @@ const Projects = () => {
         <SimpleGrid columns={[1]} spacing={[10,20,30]}>
 <Heading textAlign={'center'} fontWeight={'medium'} fontSize={'2xl'} >Pari The Doctor</Heading>
         <Video embedId={'PkCiwrQ_x9Y'}/>
-        <Text textAlign={'center'} mx={40}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
+        <Text textAlign={'center'} mx={[10,20,30,40]}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
 <Heading textAlign={'center'} fontWeight={'medium'} fontSize={'2xl'} >Emergency Relief Network</Heading>
         <Video embedId={'yLQXJM9fixw'}/>
-        <Text textAlign={'center'} mx={40}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
+        <Text textAlign={'center'} mx={[10,20,30,40]}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
 <Heading textAlign={'center'} fontWeight={'medium'} fontSize={'2xl'} >WhiteHat Preview File</Heading>
         <Video embedId={'jY3qgjkLKP0'}/>
-        <Text textAlign={'center'} mx={40}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
+        <Text textAlign={'center'} mx={[10,20,30,40]}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
 <Heading textAlign={'center'} fontWeight={'medium'} fontSize={'2xl'} >#GTDHorses - Pradeep Gill</Heading>
         <Video embedId={'OCS6Bc0ZTXc'}/>
-        <Text textAlign={'center'} mx={40}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
+        <Text textAlign={'center'} mx={[10,20,30,40]}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusantium odit illum vitae sint explicabo mollitia, nihil distinctio ipsum quas, quo, ad assumenda recusandae fugiat aliquam porro facilis reprehenderit ratione minus! Velit cumque, architecto deserunt qui nesciunt omnis doloribus.</Text>
         </SimpleGrid>
       </Flex>
     </Center>
