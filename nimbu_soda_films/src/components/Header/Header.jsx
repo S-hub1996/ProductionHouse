68098109a6b5f27f314bb20e8a9176ced0d9252c
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-08-03 16:34:52 +0530

    new logo added to header

--- nimbu_soda_films/src/components/Header/Header.jsx
+++ nimbu_soda_films/src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { FaMoon, FaSun } from "react-icons/fa";
-
+import logo from '../../assets/logo.png'
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
@@ -30,8 +30,8 @@ const Header = () => {
             color={"white"}
           >
             <Image
-              src="https://scontent-del1-1.xx.fbcdn.net/v/t1.6435-9/84978592_506324163410092_4606398471304380416_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SuTYMy6vPSUAX_Tt8OO&_nc_ht=scontent-del1-1.xx&oh=00_AT8h3EgA3oSrVMEke_j-o_lrLFxwuNl0dXF9hd66RO8x-g&oe=63063D83"
-              w={16}
+              src={logo}
+              w={24}
             />
           </Heading>
         </Link>
